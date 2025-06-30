ff852fac986f7964a4d6bdc60512737a9e7ceac5
Kamil Krecisz <zuparomanav2@gmail.com>
2025-06-30 18:07:55 +0200

    Commit 5.5 - Added settings button functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,17 @@
 extends Control
-
+@onready var settings_canvas: CanvasLayer = $CanvasLayer
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_settings_pressed():
-	print("Settings pressed")
+	settings_canvas.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_closing_button_pressed() -> void:
+	settings_canvas.visible = false
