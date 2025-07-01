b7bd0fd31f718078b061d41b7dd593ab09209eeb
Kamil Krecisz <zuparomanav2@gmail.com>
2025-07-01 17:09:48 +0200

    Commit 6.5 - Added sound effect for buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,24 +1,31 @@
 extends Control
 @onready var settings_canvas: CanvasLayer = $CanvasLayer
 @onready var AniPlayer: AnimationPlayer = $AnimationPlayer
+@onready var click_player: AudioStreamPlayer2D = $ClickPlayer
 
 
 func _on_start_pressed():
+	click_player.play()
+	await get_tree().create_timer(0.05).timeout
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_settings_pressed():
+	click_player.play()
 	if settings_canvas.visible == true:
-		return 1
+		return
 	settings_canvas.visible = true
 	AniPlayer.play("settings_animation")
 
 
-func _on_exit_pressed() -> void:
+func _on_exit_pressed():
+	click_player.play()
+	await get_tree().create_timer(0.05).timeout
 	get_tree().quit()
 
 
 func _on_closing_button_pressed():
+	click_player.play()
 	AniPlayer.play_backwards("settings_animation")
 	await get_tree().create_timer(0.2).timeout
 	settings_canvas.visible = false
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,11 +1,23 @@
 extends Control
 @onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var click_player: AudioStreamPlayer2D = $ClickPlayer
+
+func _ready():
+	$PanelContainer/VBoxContainer/Exit.disabled = true
+	$PanelContainer/VBoxContainer/Resume.disabled = true
+	$PanelContainer/VBoxContainer/MainMenu.disabled = true
 
 func resume():
+	$PanelContainer/VBoxContainer/Exit.disabled = true
+	$PanelContainer/VBoxContainer/Resume.disabled = true
+	$PanelContainer/VBoxContainer/MainMenu.disabled = true
 	get_tree().paused = false
 	animation.play_backwards("blur")
 	
 func pause():
+	$PanelContainer/VBoxContainer/Exit.disabled = false
+	$PanelContainer/VBoxContainer/Resume.disabled = false
+	$PanelContainer/VBoxContainer/MainMenu.disabled = false
 	get_tree().paused = true
 	animation.play("blur")
 	
@@ -16,13 +28,18 @@ func Esc():
 		resume()
 
 func _on_resume_pressed():
+	click_player.play()
 	resume()
 
 func _on_main_menu_pressed():
+	click_player.play()
+	await get_tree().create_timer(0.05).timeout
 	resume()
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _on_exit_pressed():
+	click_player.play()
+	await get_tree().create_timer(0.05).timeout
 	get_tree().quit()
 
 func _process(_delta):
