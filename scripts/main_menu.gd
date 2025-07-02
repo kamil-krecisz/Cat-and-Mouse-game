62c958d5fa9d9d4be8e30f861025e7d2113a6af9
Kamil Krecisz <zuparomanav2@gmail.com>
2025-07-02 20:38:56 +0200

    Commit 9.0 - Added Resolution and window mode settings

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,41 @@ extends Control
 @onready var click_player: AudioStreamPlayer2D = $ClickPlayer
 
 
+#SETTINGS BUTTONS
+@onready var resolution_option: OptionButton = $CanvasLayer/Panel/ResolutionOption
+@onready var window_mode: OptionButton = $CanvasLayer/Panel/WindowMode
+@onready var apply_button: Button = $CanvasLayer/Panel/ApplyButton
+
+
+var resolutions = [
+	Vector2i(1280, 720), Vector2i(1920, 1080),
+	Vector2i(1600, 900), Vector2i(960, 640)]
+
+
+func _ready():
+	var config = ConfigFile.new()
+	if config.load("user://settings.cfg") == OK:
+		var res = config.get_value("display", "resolution", Vector2i(1280, 720))
+		var mode = config.get_value("display", "mode", 0)
+		DisplayServer.window_set_size(res)
+		DisplayServer.window_set_mode(mode)
+	for res in resolutions:
+		resolution_option.add_item("%sx%s" % [res.x, res.y])
+		
+	window_mode.add_item("Windowed")
+	window_mode.add_item("Closed Window")
+	window_mode.add_item("Full-screen")
+	window_mode.add_item("Borderless Full-Screen")
+
+
+
+
+
+
+
+
+
+
 func _on_start_pressed():
 	click_player.play()
 	await get_tree().create_timer(0.05).timeout
@@ -29,3 +64,17 @@ func _on_closing_button_pressed():
 	AniPlayer.play_backwards("settings_animation")
 	await get_tree().create_timer(0.2).timeout
 	settings_canvas.visible = false
+
+
+func _on_apply_button_pressed():
+	var res = resolutions[resolution_option.selected]
+	print(res)
+	var mode = window_mode.selected
+	
+	DisplayServer.window_set_size(res)
+	DisplayServer.window_set_mode(mode)
+	
+	var config = ConfigFile.new()
+	config.set_value("display", "resolution", res)
+	config.set_value("display", "mode", mode)
+	config.save("user://settings.cfg")
