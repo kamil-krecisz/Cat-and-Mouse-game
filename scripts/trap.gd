e0a0423ab8a9964487d53ac9b067d15bd62d73c8
kamil-krecisz <zuparomanav2@gmail.com>
2025-07-18 16:46:19 +0200

    Commit 10.1 - Started adding points system

--- scripts/trap.gd
+++ scripts/trap.gd
@@ -3,9 +3,14 @@ extends Area2D
 @onready var timer: Timer = $Timer
 var mouse  # a variable for mouse to use in different functions(on timer timeout)
 
+@onready var label: Label = $"../CanvasLayer/Label"
+var points = 0
+
 func _on_body_entered(body):
 	if body.is_in_group("Mouse"):
 		print("Trapped")
+		points += 1
+		label.text = "Points: " + str(points)
 		body.can_move = false  # lock the movement
 		mouse = body   # save mouse to the mouse variable for later use
 		timer.start()          # start timer
